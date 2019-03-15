82043fa3fd36b2ad70260be9cd1c6300b3a44348
Naveen <withsnaveen@gmail.com>
2019-03-15 11:28:15 +0530

    add config

---
+++ config.js
@@ -0,0 +1,6 @@
+// Replace clientId and clientSecret with values obtained by you for your
+// application https://github.com/settings/applications.
+var clientId = 'PutClientIdHere';
+// Note that in a real-production app, you may not want to store
+// clientSecret in your App code.
+var clientSecret = 'PutClientSecretHere';
